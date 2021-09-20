d8c105f515aa500de28b2b8a34b680792369a585
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-09-20 15:04:03 +0900

    create four video screen

--- src/components/VideoRemoteThree.js
+++ src/components/VideoRemoteThree.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoRemote = ({peerManage}) => {
   // TODO リファクタ対象
-  const videoRef = peerManage.remoteVideoRef;
+  const videoRef = peerManage.remoteVideoRefThree;
 
   return(
     <video autoPlay muted={false} ref={videoRef}/>
